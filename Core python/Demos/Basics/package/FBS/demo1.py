f0db376837a69ec412d83f7d0155a45c61ef0e05
shrutikajadhav2512 <shrutikajadhav2512@gmail.com>
2026-09-13 16:00:14 +0530

    Add files via upload

---
+++ Core python/Demos/Basics/package/FBS/demo1.py
@@ -0,0 +1,5 @@
+def add():
+    print("I am in Addition")
+def sub():
+    print("I am in Substraction")
+a=25
---
+++ Core python/Demos/Basics/package/main.py
@@ -0,0 +1,12 @@
+# import FBS.demo1
+# FBS.demo1.add()
+# from FBS.demo1 import add
+# add()
+# from FBS.demo1 import add,sub
+# add()
+# sub()
+# from FBS.demo1 import *
+# add()
+# print(a)
+import FBS.demo1 as f
+f.add()
